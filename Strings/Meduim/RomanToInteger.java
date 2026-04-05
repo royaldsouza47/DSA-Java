d51a3fd83889d60d72619f08eea70a003e39b6b2
Royal Dsouza <royaltondsouza081@gmail.com>
2026-04-05 23:34:18 +0530

    Day 43 - Solved RemoveOutermostParentheses | Strings | Total: 42 problems

--- Strings/Meduim/RomanToInteger.java
+++ Strings/Meduim/RomanToInteger.java
@@ -1,31 +1,29 @@
-<<<<<<< HEAD
 import java.util.HashMap;
 
 public class RomanToInteger {
 
     public static int roman(String s){
         HashMap<Character,Integer> map=new HashMap<>();
-        map.put('I', 1);
+         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
         map.put('L', 50);
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
-
         int result=0;
+        
         for(int i=0;i<s.length();i++){
             int current=map.get(s.charAt(i));
-            if (i+1<s.length()) {
+            if(i+1<s.length()){
                 int next=map.get(s.charAt(i+1));
                 if (current<next) {
-                    result-=current;
-                }
-                else{
-                    result+=current;
-                }
-                
+                result-=current;
             }else{
+                result+=current;
+            }
+            }
+            else{
                 result+=current;
             }
         }
@@ -34,43 +32,4 @@ public class RomanToInteger {
     public static void main(String[] args) {
         System.out.println(roman("XIV"));
     }
-    
-=======
-import java.util.HashMap;
-
-public class RomanToInteger {
-
-    public static int roman(String s){
-        HashMap<Character,Integer> map=new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
-        int result=0;
-        for(int i=0;i<s.length();i++){
-            int current=map.get(s.charAt(i));
-            if (i+1<s.length()) {
-                int next=map.get(s.charAt(i+1));
-                if (current<next) {
-                    result-=current;
-                }
-                else{
-                    result+=current;
-                }
-                
-            }else{
-                result+=current;
-            }
-        }
-        return result;
-    }
-    public static void main(String[] args) {
-        System.out.println(roman("XIV"));
-    }
-    
->>>>>>> 9666f7e7bcaa91c334e661a684c6060df61c4f75
 }
